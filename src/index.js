0060bfe65501eebb7aadee3c7762e2977491f35d
samanthasieversdesign-ai <samanthasieversdesign@gmail.com>
2026-02-17 18:21:32 -0600

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import BookLodgeApp from './BookLodge';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <BookLodgeApp />
+  </React.StrictMode>
+);
